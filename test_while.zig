9bdd3f593efb08d5daed4a7c8977fdbb9094d7a3
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-09 11:05:09 -0500

    while loops

---
+++ test_while.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "while" {
+    var i: u8 = 2;
+    // your standard while loop
+    while (i < 100) {
+        i *= 2;
+    }
+    try expect(i == 128);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    // basically a makeshift for loop
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+    try expect(sum == 55);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 3) : (i += 1) {
+        // there is also the standard `continue` statement
+        if (i == 2) continue;
+        sum += 1;
+    }
+    try expect(sum == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+    try expect(sum == 1);
+}
