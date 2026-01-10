7137b9a03fedb6198d3db9719b49d56ca71fa9e9
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-10 11:10:03 -0500

    add comments to switch test file

--- test_switch.zig
+++ test_switch.zig
@@ -4,11 +4,12 @@ const expect = std.testing.expect;
 test "switch statement" {
     var x: i8 = 10;
     switch (x) {
+        // Range prong uses `a...b` inclusive syntax.
         -1...1 => {
             x = -x;
         },
         10, 100 => {
-            // Special considerations must be made when dividing signed integers
+            // `@divExact` traps if remainder is non-zero.
             x = @divExact(x, 10);
         },
         else => {},
@@ -19,7 +20,7 @@ test "switch statement" {
 
 test "switch expression" {
     var x: i8 = 10;
-    // More concise, following pattern matching patterns
+    // `switch` can be an expression with a result value.
     x = switch (x) {
         -1...1 => -x,
         10, 100 => @divExact(x, 10),
