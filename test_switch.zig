c39907d3de773610311fa61f32df5f87b4de3c3d
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-10 11:03:39 -0500

    switch

---
+++ test_switch.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "switch statement" {
+    var x: i8 = 10;
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+        10, 100 => {
+            // Special considerations must be made when dividing signed integers
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+
+    try expect(x == 1);
+}
+
+test "switch expression" {
+    var x: i8 = 10;
+    // More concise, following pattern matching patterns
+    x = switch (x) {
+        -1...1 => -x,
+        10, 100 => @divExact(x, 10),
+        else => x,
+    };
+    try expect(x == 1);
+}
