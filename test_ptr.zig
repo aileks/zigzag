f4b1c8eaac0ca997e1a445dde464e6734675ddac
Aaliyah Harper <aaliyah@aileks.dev>
2026-01-20 18:26:35 -0500

    pointers

---
+++ test_ptr.zig
@@ -0,0 +1,24 @@
+const expect = @import("std").testing.expect;
+
+fn increment(num: *u8) void {
+    num.* += 1;
+}
+
+test "pointers" {
+    var x: u8 = 1;
+    increment(&x);
+    try expect(x == 2);
+}
+
+// test "naughty pointer" {
+//     var x: u16 = 5;
+//     x -= 5;
+//     var y: *u8 = @ptrFromInt(x);
+//     y = y;
+// }
+
+// test "conts pointers" {
+//     const x: u8 = 1;
+//     var y = &x;
+//     y.* += 1;
+// }
